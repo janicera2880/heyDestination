691a5c2d0b0a5617fde4ca221d3bbe8ddd914ad5
Janice <janiceralecha@gmail.com>
2023-06-07 00:43:51 -0500

    Validation and Association setup for Location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,19 @@
 class Location < ApplicationRecord
+
+    has_many :villas # Each location has many villas
+    has_many :inquiries # Each location has many inquiries
+    has_many :activity_locations # Each location has many ActivityLocations
+    has_many :activities, through: :activity_locations # Each location has many activities through ActivityLocations
+
+    # Validations
+    validates :city, :state, :country, presence: true, length: { maximum: 50 }
+    validates :description, length: { maximum: 500 }
+    validates :image, presence: true
+    validate :validate_state_belongs_to_country
+
+
+    def validate_state_belongs_to_country
+        # Custom validation logic to check if the state belongs to the country
+        errors.add(:state, "must belong to the selected country") unless State.where(country: country).exists?
+    end
 end
--- app/models/user_admin.rb
+++ app/models/user_admin.rb
@@ -1,12 +1,12 @@
 class UserAdmin < ApplicationRecord
-    has_secure_password
+    has_secure_password # Provided by the bcrypt gem to handle secure password storage and authentication.
 
     has_many :villas
 	has_many :inquiries, through: :villas
 	
-    validates :first_name, :last_name, presence: true
-    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :phone_number, presence: true, format: { with: /\A\d{10}\z/, message: "should be a 10-digit number" }
-    validates :password, length: { in: 8..20 }
-    validates :admin, presence: true, inclusion: { in: [true, false] }
+    validates :first_name, :last_name, presence: true # Validates presence of first_name and last_name
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } # Validates presence and format of email using a regular expression
+    validates :phone_number, presence: true, format: { with: /\A\d{10}\z/, message: "should be a 10-digit number" } # Validates presence and format of phone_number using a regular expression
+    validates :password, length: { in: 8..45 } # Validates length of password between 8 and 45 characters
+    validates :admin, presence: true, inclusion: { in: [true, false] } # Validates presence and inclusion of admin, which should be either true or false
 end
