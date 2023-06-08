1953f5cb881a7383cfbd2a6bd873360cec4daff0
Janice <janiceralecha@gmail.com>
2023-06-08 02:59:38 -0500

    Fix syntax error

--- app/serializers/villa_serializer.rb
+++ app/serializers/villa_serializer.rb
@@ -1,5 +1,5 @@
 class VillaSerializer < ActiveModel::Serializer
-  attributes :id, :name, :highlights, :features, :services, :overview, :features, :amenities, :rate, :capacity, :bedroom, :bathroom, :image, , :user_admin_id, :location_id
+  attributes :id, :name, :highlights, :features, :services, :overview, :features, :amenities, :rate, :capacity, :bedroom, :bathroom, :image, :user_admin_id, :location_id
 
   belongs_to :user
   belongs_to :location
