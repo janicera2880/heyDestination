68256853c5a4b20c0542243f90ef8fbde540b7f6
Janice <janiceralecha@gmail.com>
2023-06-07 20:00:27 -0500

    Specify validation and associations for models Activity and ActivityLocations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,16 @@
 class Activity < ApplicationRecord
+
+    # Associations
+    # Has a one-to-many association with the ActivityLocation model. 
+    # This means that an activity can have multiple activity locations.
+    has_many :activity_locations
+	has_many :locations, through: :activity_locations
+    # Specifies a many-to-many association between the Activity model and the Location model through the ActivityLocation model.
+    # This association allows an activity to be associated with multiple locations indirectly through the activity_locations table.
+    
+    # Validations
+    validates :name, presence: true, length: { maximum: 100 }
+    validates :highlights, :details, presence: true, length: { minimum: 50 }
+    validates :image, presence: true
+    validates :categories, presence: true, inclusion: { in: ['Sports', 'Lifestyle', 'Events', 'Unique'], message: "must be one of: Sports, Lifestyle, Events, Unique" }
 end
--- app/models/activity_location.rb
+++ app/models/activity_location.rb
@@ -1,2 +1,8 @@
 class ActivityLocation < ApplicationRecord
+
+    # Represents a joined table in a many-to-many relationship between the Activity and Location models.
+
+    belongs_to :activity
+	belongs_to :location
+
 end
