410cf628efeecca7104b2163433ade7c2a3b0a5a
Janice <janiceralecha@gmail.com>
2023-06-08 07:50:21 -0500

    Create route that allows to post an inquiry will be and will only support the post HTTP method.

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,2 +1,18 @@
 class InquiriesController < ApplicationController
+
+    def create
+        inquiry = Inquiry.new(inquiry_params)
+        if inquiry.save
+          render json: inquiry, status: :created
+        else
+          render json: { errors: inquiry.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+    
+      private
+    
+      def inquiry_params
+        params.require(:inquiry).permit(:arrival, :departure, :guests, :full_name, :email, :phone, :message)
+      end
+    end
 end
