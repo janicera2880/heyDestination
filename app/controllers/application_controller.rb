ee1b8cfa73588f0da158097f3ccf0730f9a83ed4
Janice <janiceralecha@gmail.com>
2023-06-09 13:35:20 -0500

    Use rescue method to handle exceptions and render errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_response 
+
+  # Ensuring that users are authorized to perform the actions they request, prevent unauthorized access.
+  before_action :authorize
+
+  private
+
+  def authorize
+    @current_user = UserAdmin.find_by(id: session[:user_admin_id])
+
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def render_record_not_found_response(invalid)
+    render json: { errors: invalid }, status: :not_found 
+  end
+
 end
