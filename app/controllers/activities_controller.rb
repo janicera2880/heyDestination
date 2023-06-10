3ae8a30fc35c53c128d5791c0e598d9b1d25beef
Janice <janiceralecha@gmail.com>
2023-06-10 11:37:15 -0500

    Define CRUD actions

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,32 @@
 class ActivitiesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    before_action :authorize
+    skip_before_action :authorize, only: [:index, :show]
+
+    # GET /activities
+    def index
+        activities = Activities.all
+        render json: activities
+    end
+    # GET /activities/:id
+    def show
+        activity = Activities.find(params[:id])
+        render json: activity
+    end
+
+    # POST /activities
+    def create
+        activity = Activities.create!(activity_params)
+        render json: activity, status: :created
+    end
+
+    private
+    def activity_params
+        params.permit(:name, :highlights, :image, :details, :categories)
+    end
+
+    def render_not_found_response
+        render json: { error: "Activity not found" }, status: :not_found
+    end
+
 end
--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,6 +1,7 @@
 class InquiriesController < ApplicationController
 
     def create
+      #Create a new instance of Inquiry using the inquiry_params method.
         inquiry = Inquiry.new(inquiry_params)
         if inquiry.save
           render json: inquiry, status: :created
@@ -10,7 +11,8 @@ class InquiriesController < ApplicationController
     end
     
       private
-    
+      
+      # Retrieve and require the :inquiry parameter from the params object, and permit specific attributes.
       def inquiry_params
         params.require(:inquiry).permit(:arrival, :departure, :guests, :full_name, :email, :phone, :message)
       end
