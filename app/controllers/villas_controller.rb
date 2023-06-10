f28b99cf95cd727aa2ab1999efed690d0e8969db
Janice <janiceralecha@gmail.com>
2023-06-10 12:19:05 -0500

    Define CRUD operations,exceptions and error messages.

--- app/controllers/villas_controller.rb
+++ app/controllers/villas_controller.rb
@@ -1,2 +1,76 @@
 class VillasController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    before_action :authorize
+    skip_before_action :authorize, only: [:index, :show]
+
+    # GET /villas
+    def index
+        villas = Villas.all
+        render json: villas, include: ['locations.activity_locations']
+    end
+    # GET /villas/:id
+    def show
+        villa = find_villa
+        render json: villa, status: :ok
+    end
+
+    # POST /villas
+    def create
+        user_admin = find_user_admin
+        if authorized_user?(user_admin)
+        new_villa = user_admin.villas.create!(villa_params)
+        render json: new_villa, status: :created
+        else
+            render json: { errors: user_admin.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    # PATCH /villas/:id
+    def update
+        villa = find_villa
+        if authorized_user?(villa)
+            villa.update!(villa_params)
+            render json: villa, status: :ok
+        else
+            render json: { errors: villa.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    # DELETE /villas/:id
+    def destroy
+        villa = find_villa
+        if authorized_user?(villa)
+            villa.destroy
+            head :no_content
+        else
+            render json: { error: "You are not authorized to delete this post" }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def villa_params
+        params.permit(:name, :highlights, :services, :overview, :features, :amenities, :rate, :capacity, :bedroom, :bathroom, :image, :user_admin_id, :location_id)
+    end
+
+    def find_villa
+        Villas.find(params[:id])
+    end
+
+    def find_user_admin
+        UserAdmin.find(session[:user_admin_id])
+    end
+
+    def render_not_found_response
+        render json: { error: 'Villa Not Found' }, status: :not_found
+    end
+
+    def authorize
+        render json: { errors: ['Not Authorized'] }, status: :unauthorized unless session.include? :user_admin_id
+    end
+
+    def authorized_user?(villa)
+        villa.user_admin_id == session[:user_admin_id]
+    end
+
 end
