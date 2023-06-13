f325f29d226802d9131028219ff650cb096681fa
Janice <janiceralecha@gmail.com>
2023-06-13 13:30:11 -0500

    Revise methods in serializer.

--- app/serializers/villa_serializer.rb
+++ app/serializers/villa_serializer.rb
@@ -1,7 +1,11 @@
 class VillaSerializer < ActiveModel::Serializer
-  attributes :id, :name, :highlights, :features, :services, :overview, :features, :amenities, :rate, :capacity, :bedroom, :bathroom, :image, :user_admin_id, :location_id
+  attributes :id, :name, :highlights, :features, :services, :overview, :features, :amenities, :rate, :capacity, :bedroom, :bathroom, :user_admin_id, :location_id, :image_url
 
   belongs_to :user
   belongs_to :location
   has_many :inquiries
+
+  def image_url
+    object.image.service_url if object.image.attached?
+  end
 end
