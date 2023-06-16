fd307bc2b21514b867ca60e2041ded58084d3c8f
Janice <janiceralecha@gmail.com>
2023-06-16 06:02:57 -0500

    fix error

--- client/src/Components/LocationsContainer.js
+++ client/src/Components/LocationsContainer.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { LocationsContext } from '../Contexts/LocationsContext';
 import LocationsCard from './LocationsCard';
 import AddLocationForm from './AddLocationForm';
-import { UserContext } from '../Contexts/UserContext';
+import { UserAdminContext } from '../Contexts/UserAdminContext';
 
 // Returns a container component that displays a list of locations.
 const LocationsContainer = () => {
