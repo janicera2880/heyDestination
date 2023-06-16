b9c1be549a1e3d744856e7bd07011ad1007cc486
Janice <janiceralecha@gmail.com>
2023-06-15 22:54:48 -0500

    Define a function to add new location using useContext

--- client/src/Components/LocationsContainer.js
+++ client/src/Components/LocationsContainer.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { LocationsContext } from '../Contexts/LocationsContext';
 import LocationsCard from './LocationsCard';
 
+//Returns a container component that displays a list of locations.
 const LocationsContainer = () => {
   const { locations } = useContext(LocationsContext);
 
--- client/src/Contexts/LocationsContext.js
+++ client/src/Contexts/LocationsContext.js
@@ -3,11 +3,17 @@ import React, { useState } from "react";
 const LocationsContext = React.createContext();
 
 function LocationsProvider({ children }){
+// State for storing the locations
+const [locations, setLocations] = useState([]);
 
-    const [locations, setLocations] = useState(null);
-   
+// Function to add a new location
+const addLocation = (newLocation) => {
+    setLocations([...locations, newLocation]);
+  };
+
+  // Context provider component
     return(
-        <LocationsContext.Provider value={{locations, setLocations}}>
+        <LocationsContext.Provider value={{locations, addLocation}}>
             {children}
         </LocationsContext.Provider>
     );
