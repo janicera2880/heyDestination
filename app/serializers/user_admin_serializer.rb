815d3e35ca53a09737821593f8fcc23ccad293a1
Janice <janiceralecha@gmail.com>
2023-06-19 17:16:10 -0500

    Try fix no method error

--- app/serializers/user_admin_serializer.rb
+++ app/serializers/user_admin_serializer.rb
@@ -1,7 +1,12 @@
-class UserAdminSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :phone, :admin
+cclass UserAdminSerializer < ActiveModel::Serializer
+attributes :id, :first_name, :last_name, :email, :phone, :admin, :profile_pic_url
 
-  has_many :villas
-  has_many :inquiries, through: :villas
-  has_one_attached :profile_pic
+has_many :villas
+has_many :inquiries, through: :villas
+
+def profile_pic_url
+  if object.profile_pic.attached?
+    Rails.application.routes.url_helpers.rails_blob_url(object.profile_pic)
+  end
+end
 end
