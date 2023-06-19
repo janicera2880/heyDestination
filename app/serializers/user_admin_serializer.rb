409ba6c6adfa6c243234760a7bd51e5af78d2aa7
Janice <janiceralecha@gmail.com>
2023-06-19 17:27:59 -0500

    Debug

--- app/serializers/user_admin_serializer.rb
+++ app/serializers/user_admin_serializer.rb
@@ -4,9 +4,9 @@ attributes :id, :first_name, :last_name, :email, :phone, :admin, :profile_pic_ur
 has_many :villas
 has_many :inquiries, through: :villas
 
-def profile_pic_url
-  if object.profile_pic.attached?
+  def profile_pic_url
+    if object.profile_pic.attached?
     Rails.application.routes.url_helpers.rails_blob_url(object.profile_pic)
+    end
   end
-
 end
