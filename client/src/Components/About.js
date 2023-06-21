6dd8a1c023e8cac5fce4b6985f0e6cfca95b4fcd
Janice <janiceralecha@gmail.com>
2023-06-21 06:17:58 -0500

    Add content to About Us Page

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -2,23 +2,37 @@ import React, { useContext } from "react";
 import { UserAdminContext } from "../Contexts/UserAdminContext";
 import { Link } from "react-router-dom";
 import LoginSignupToggle from "./LoginSignupToggle";
+import ReactMarkdown from "react-markdown";
+import villaPicture from "./Images/travel.png";
+
 
 const AboutUs = () => {
   const { userAdmin } = useContext(UserAdminContext);
 
   return (
     <div className="about-us">
-      <h2>Your Passport To Luxury Villas</h2>
-      <p>
-        Welcome to Luxury Villa Rentals! We offer a wide selection of luxurious villas in beautiful destinations around the world. Whether you're planning a romantic getaway, a family vacation, or a special event, we have the perfect villa to suit your needs.
-      </p>
-      <p>
-        Our team of experienced travel experts is dedicated to providing exceptional service and ensuring that your stay in our villas is nothing short of extraordinary. With stunning locations, exquisite amenities, and personalized concierge services, we strive to create unforgettable experiences for our guests.
-      </p>
-      <p>
-        Browse our villa collection, explore the breathtaking destinations, and start planning your dream vacation today. If you have any questions or need assistance, feel free to contact our friendly customer support team. We can't wait to help you create memories that will last a lifetime.
-      </p>
-      
+
+        <img
+        src={villaPicture}
+        width="700"
+        height="500"
+        alt="villa"
+        className="villa-pic" />
+      <ReactMarkdown>## Discover Your Perfect Villa Retreat</ReactMarkdown>
+      <ReactMarkdown>At **hey-Destination**, it's our goal to make luxury vacation planning as effortless as the escape itself.</ReactMarkdown>
+      <p>Renting a villa is simple, and our expert villa specialists are available to guide you every step of the way.</p>
+      <p>Our team of experienced travel experts is dedicated to providing *exceptional* service and ensuring that your stay in our villas is nothing short of *extraordinary*.</p>
+      <ReactMarkdown>### What Makes A Luxury Villa Vacation</ReactMarkdown> 
+      <ReactMarkdown>- **Features & Amenities**
+        We go above and beyond entertainment and surround sound. It's about private gyms, spas, children’s spaces, al fresco dining, private gyms, and beyond. Nothing is too big or too extravagant for our guests.</ReactMarkdown> 
+        <ReactMarkdown>- **Staff & Services**
+        We offer and fully guarantee that our Concierge Services will take the guesswork out of your getaway. Additionally, many of our villas feature private chefs, butlers, gardeners, and more to elevate your experience.</ReactMarkdown> 
+        <ReactMarkdown>- **Superior Design & Maintenance**
+        We know that overall aesthetics matter, but it goes beyond that. All our villas feature impeccable landscaping and views, outdoor spaces, interior design and furnishings, and more. We also ensure that each villa is personally inspected and vetted, and every villa is maintained from top to bottom.</ReactMarkdown> 
+        <ReactMarkdown>### We Are More Than Just A Villa Rental</ReactMarkdown>
+        <ReactMarkdown>At **hey-Destination**, every villa reservation includes our personal villa Concierge Service. Your *Concierge* is committed to providing around-the-clock personalized services to ensure every last detail of your getaway – no matter how large or small – is provided.</ReactMarkdown>
+<ReactMarkdown>Explore the local attractions, national parks, best beaches, and more. It's our pleasure to provide you with everything you need to craft a truly unique experience designed around your wants and wishes. Let us make restaurant reservations, and allow us to handle tricky last-minute itinerary changes. Your villa Concierge takes care of all the details from start to finish making certain that your escape is one-of-a-kind.</ReactMarkdown>
+
       {!userAdmin ? (
         <div className="login-section">
           <h3>Ready to book your villa?</h3>
@@ -27,7 +41,7 @@ const AboutUs = () => {
         </div>
       ) : (
         <div className="admin-link">
-          <p>Logged in as {userAdmin.first_name} {userAdmin.last_name}</p>
+          <h4>Logged in as {userAdmin.first_name} {userAdmin.last_name}</h4>
           <Link to="/user_admin">Go to Admin Portal</Link>
         </div>
       )}
