91e1daf8d9b1397af4beb5011ece94922e437cab
Janice <janiceralecha@gmail.com>
2023-07-06 15:24:44 -0500

    Change app icon and tab title

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,7 +4,7 @@ class LocationsController < ApplicationController
 
     # Fetch all locations from the database and order them by ID.
     def index
-        locations = Location.all.order(:id)
+        locations = Location.all.order(id: :desc)
         render json: locations, include: ['villas', 'inquiries', 'activity_locations', 'activities'], status: :ok
     end
 
